836b352f1e7f6b6e34b5cee0e71f2a7e66c01759
Anderson Hansen <anderson.hansen@dbccompany.com.br>
2022-09-01 08:40:02 -0300

    Criação do projeto

---
+++ README.md
@@ -0,0 +1,10 @@
+# dotnet-aws-sns-sqs
+Demonstração de como podemos implementar mensageria utilizando Amazon SQS (Queue) e Amazon SNS (Tópico).
+
+# Como executar:
+- Clonar / baixar o repositório em seu workplace.
+- Baixar o .Net Core SDK e o Visual Studio Code mais recentes.
+- Alterar no projeto as credenciais AWS_ACCESS_KEY e AWS_SECRET_KEY de acordo com sua conta AWS.
+
+# Sobre
+Este projeto foi desenvolvido por Anderson Hansen sob [MIT license](LICENSE).
---
+++ src/services/AmazonQueue.Publisher/Program.cs
@@ -0,0 +1,31 @@
+﻿using AmazonQueue.MessageBus;
+using AmazonQueue.MessageBus.Integration;
+
+namespace AmazonQueue.Publisher
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var busService = new MessageBusService("__AWS_ACCESS_KEY__", "__AWS_SECRET_KEY__");
+
+            while (true)
+            {
+                Console.Write("Código do Carrinho: ");
+
+                int id = int.Parse(Console.ReadLine());
+
+                // Queue
+                var evt = new CartCreatedIntegrationEvent(id, "Anderson");
+                var queueUrl = await busService.GetQueueUrl(QueueTypes.AWS_SQS_CART_CREATED);
+                await busService.SendAsync(queueUrl, evt);
+
+                // Tópico
+                // var evt = new UserCreatedIntegrationEvent(id);
+                // var topicArn = await busService.GetTopicArn(TopicTypes.AWS_SNS_USER_CREATED);
+                // await busService.PublishAsync(topicArn, evt);
+
+            }
+        }
+    }
+}
